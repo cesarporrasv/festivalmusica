a860e5560be265ce7bc26a5e0dc69eca4a104ef2
César <cesar@MacBook-Pro-de-Cesar.local>
2025-03-20 08:50:04 -0500

    añadido galeria y js

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  crearGaleria();
+});
+
+function crearGaleria() {
+  const cantidadImagenes = 16;
+  const galeria = document.querySelector(".galeria-imagenes");
+
+  for (let i = 1; i <= cantidadImagenes; i++) {
+    const imagen = document.createElement("IMG");
+    imagen.src = `src/img/gallery/full/${i}.jpg`;
+    imagen.alt = "imagen galeria";
+
+    galeria.appendChild(imagen);
+  }
+}
