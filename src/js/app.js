431e2f29317ec5cf9b5c73116a4215e0c7f14ad7
César <cesar@MacBook-Pro-de-Cesar.local>
2025-03-21 08:42:13 -0500

    añadido modal

--- src/js/app.js
+++ src/js/app.js
@@ -11,6 +11,39 @@ function crearGaleria() {
     imagen.src = `src/img/gallery/full/${i}.jpg`;
     imagen.alt = "imagen galeria";
 
+    //Event Handler
+    imagen.onclick = function () {
+      mostrarImagen(i);
+    };
+
     galeria.appendChild(imagen);
   }
+}
+
+function mostrarImagen(i) {
+  const imagen = document.createElement("IMG");
+  imagen.src = `src/img/gallery/full/${i}.jpg`;
+  imagen.alt = "imagen galeria";
+
+  //Generar el Modal
+  const modal = document.createElement("DIV");
+  modal.classList.add("modal");
+  modal.onclick = cerrarModal;
+  modal.appendChild(imagen);
+
+  //Agregar al HTML
+  const body = document.querySelector("body");
+  body.classList.add("no-scroll");
+  body.appendChild(modal);
+}
+
+function cerrarModal() {
+  const modal = document.querySelector(".modal");
+  modal.classList.add("fade-out");
+
+  setTimeout(() => {
+    modal?.remove();
+    const body = document.querySelector("body");
+    body.classList.remove("no-scroll");
+  }, 280);
 }
