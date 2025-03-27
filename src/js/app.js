56fd1083c3bcbbc7c9f16b45e60ed72841fa06f9
César <cesar@MacBook-Pro-de-Cesar.local>
2025-03-27 08:42:35 -0500

    finished project

--- src/js/app.js
+++ src/js/app.js
@@ -23,12 +23,12 @@ function crearGaleria() {
   const galeria = document.querySelector(".galeria-imagenes");
 
   for (let i = 1; i <= cantidadImagenes; i++) {
-    const imagen = document.createElement("IMG");
-    imagen.loading = "lazy";
-    imagen.width = 300;
-    imagen.height = 200;
-    imagen.src = `src/img/gallery/thumb/${i}.jpg`;
-    imagen.alt = "imagen galeria";
+    const imagen = document.createElement("PICTURE");
+    imagen.innerHTML = `
+    <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+    <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+`;
 
     //Event Handler
     imagen.onclick = function () {
@@ -40,10 +40,12 @@ function crearGaleria() {
 }
 
 function mostrarImagen(i) {
-  const imagen = document.createElement("IMG");
-  imagen.src = `src/img/gallery/full/${i}.jpg`;
-  imagen.alt = "imagen galeria";
-
+  const imagen = document.createElement("PICTURE");
+  imagen.innerHTML = `
+  <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+  <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+  <img loading="lazy" width="200" height="300" src="build/img/gallery/full/${i}.jpg" alt="imagen galeria">
+`;
   //Generar el Modal
   const modal = document.createElement("DIV");
   modal.classList.add("modal");
